0883b0abedbcc286bd8661c3d1b25f8fadbdff0a
Gvdv <Gvdv@CP86264.gentgrp.gent.be>
2022-08-10 19:42:27 +0200

    Added styling

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
 const path = require('path');
 const express = require('express');
+const rootDir = require('../util/path');
 
 const router = express.Router();
 
 router.get('/add-date', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '../', 'views', 'admin.html'));
+    res.sendFile(path.join(rootDir, 'views', 'admin.html'));
   });
 
 
