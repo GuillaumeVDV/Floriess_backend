919349fc6af2729b01b1d7d5cab626481c77d499
GuillaumeVDV <vdvguillaume@gmail.com>
2022-08-21 14:10:36 +0200

    Changed routing

--- routes/calendar.js
+++ routes/calendar.js
@@ -3,11 +3,26 @@ const express = require('express');
 const path = require('path');
 
 const router = express.Router();
+const dates = [];
 
-router.get('/calendar', (req, res, next) => {
 
- res.sendFile(path.join(__dirname, '../', 'views', 'calendar.html'));
-
+router.get('/', (req, res, next) => {
+ 
+ res.render('calendar', {pageTitle: 'Kalender', dates: dates});
+ console.log(dates);
 });
+
+router.get('/add-date', (req, res, next) => {
+    
+    
+ res.render('add-date', {pageTitle: 'Datum toevoegen'});
+
+} );
+
+router.post('/add-date', (req, res, next) => {
+    dates.push({date : req.body.date, start : req.body.start, end : req.body.end});
+    res.redirect('/');
+} );
+
 
 module.exports = router;
