70530cb0198d39f7555690428279cdf7fec973bf
GuillaumeVDV <vdvguillaume@gmail.com>
2022-08-21 14:12:38 +0200

    Set pug to new view engine

--- app.js
+++ app.js
@@ -4,16 +4,15 @@ const path = require('path');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const calendarRoutes = require('./routes/calendar');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-
-
-app.use('/admin',adminRoutes);
-app.use('/calendar',calendarRoutes);
+app.use('/',calendarRoutes);
 
 app.use((req, res, next) => {
   res.status(404).send('<h1>Page not found</h1>');
